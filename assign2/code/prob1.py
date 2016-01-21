02f8ae0df95cd532c012d52fe14a1c82c7b8cca5
Sai Bi <soundsilencebisai@gmail.com>
2016-01-20 23:02:09 -0800

    add: code

--- assign2/code/prob1.py
+++ assign2/code/prob1.py
@@ -19,7 +19,8 @@ def calculate_pj_w(train_data, train_label):
     count = data[:, 2]
     # word_num = np.max(word_id)
     word_num = 61188
-    doc_num = np.max(doc_id)
+    # doc_num = np.max(doc_id)
+    # doc_num = train_label.shape[0]
 
     pjw = np.zeros((20, word_num), 'int64')
 
@@ -37,29 +38,34 @@ def test(test_data, test_label, train_pj, train_pjw):
     pjw = np.log(pjw)
     # pjw = np.log(1 + pjw)
 
-    test_num = np.max(test_data[:, 0])
+    # test_num = np.max(test_data[:, 0])
+    # test_num = test_label.shape[0]
+    test_doc_id = np.unique(test_data[:, 0])
+    test_num = test_doc_id.shape[0]
+
     doc_id = test_data[:, 0]
-    word_id = test_data[:,1]
+    word_id = test_data[:, 1]
     word_count = test_data[:,2]
 
     word_num = 61188
     error_count = 0
     for i in range(0, int(test_num)):
-        index = np.where(doc_id == i + 1)
+        # index = np.where(doc_id == i + 1)
+        index = np.where(doc_id == test_doc_id[i])
         freq = np.zeros(word_num)
 
         freq[word_id[index]-1] = word_count[index]
 
         total = np.sum(word_count[index])
         # freq = freq / total
-        freq = np.log(1 + freq)
+        # freq = np.log(1 + freq)
 
         error = np.zeros((20, 1))
         for j in range(0, 20):
             temp = np.multiply(freq, pjw[j, :])
             error[j] = np.sum(temp) + pj[j]
         index = np.argmax(error)
-        if index != test_label[i] - 1:
+        if index != test_label[test_doc_id[i]-1] - 1:
             error_count = error_count + 1
 
     print error_count / float(test_num)
@@ -75,21 +81,42 @@ def test(test_data, test_label, train_pj, train_pjw):
 if __name__ == '__main__':
     # Load training data
     data_dir = 'C:\\Users\\bisai\\Documents\\GitHub\\CSE250B\\data\\20news-bydate\\matlab\\'
-    # print 'Load training data...'
-    # train = np.loadtxt('%strain.data'%data_dir)
-    # print 'Load training label...'
-    # train_label = np.loadtxt('%strain.label'%data_dir)
+    print 'Load training data...'
+    train = np.loadtxt('%strain.data'%data_dir, 'int')
+    print 'Load training label...'
+    train_label = np.loadtxt('%strain.label'%data_dir, 'int')
+
+    total = train_label.shape[0]
+    # perm = np.random.permutation(train_num)
+    perm = np.load('%sperm.npy'%data_dir)
+
+
+    train_num = int(0.8 * total)
+    valid_num = total - train_num
+    # np.save('%sperm'%data_dir, perm)
+
+
+    train_index = np.in1d(train[:, 0] - 1, perm[0:train_num])
+    valid_index = np.in1d(train[:, 0] - 1, perm[train_num:])
+
+    train_label_index = perm[0:train_num]
+    valid_label_index = perm[train_num:]
+
+    pij = calculate_pi_j(train[train_index, :], train_label)
     # pij = calculate_pi_j(train, train_label)
     # np.save('%spj'%data_dir, pij)
+    pjw = calculate_pj_w(train[train_index, :], train_label)
     # pjw = calculate_pj_w(train, train_label)
     # np.save('%spjw'%data_dir, pjw)
     # exit(-1)
 
-    pj = np.load('%spj.npy'%data_dir)
-    pjw = np.load('%spjw.npy'%data_dir)
+    # pj = np.load('%spj.npy'%data_dir)
+    # pjw = np.load('%spjw.npy'%data_dir)
     print 'Load test data...'
     test_data = np.loadtxt('%stest.data'%data_dir, 'int')
     print 'Load test label...'
     test_label = np.loadtxt('%stest.label'%data_dir, 'int')
 
-    test(test_data,test_label,pj, pjw)
+    test(train[valid_index, :], train_label, pij, pjw)
+
+    test(test_data,test_label,pij, pjw)
