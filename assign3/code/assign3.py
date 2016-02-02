7f7837ef46dfac29f8012d8b43df9d7530b022e6
Sai Bi <soundsilencebisai@gmail.com>
2016-02-01 16:27:53 -0800

    add: prob 4

--- assign3/code/assign3.py
+++ assign3/code/assign3.py
@@ -19,8 +19,50 @@ def PlotBivGaussian(mean, cov_matrix):
     plt.show()
     return samples
 
+def PlotBoundary():
+    x = np.linspace(-10, 4, 500, endpoint=True)
+    y = (3 * x + 12) / 4
+
+    fig = plt.figure()
+    fig.suptitle('Problem 3', fontsize=20, fontweight='bold')
+    ax = fig.add_subplot(111)
+    ax.plot(x, y)
+
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.spines['bottom'].set_position(('data',0))
+    ax.yaxis.set_ticks_position('left')
+    ax.spines['left'].set_position(('data',0))
+
+    plt.annotate(r'$(-4,0)$',
+             xy=(-4, 0), xycoords='data',
+             xytext=(-20, +60), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+
+    plt.annotate(r'$(0,3)$',
+             xy=(0, 3), xycoords='data',
+             xytext=(+10, -30), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+
+    plt.annotate(r'positive',
+             xy=(-6, 2), xycoords='data',
+             xytext=(+10, +30), textcoords='offset points', fontsize=16)
+
+    # ax.set_xlabel("x")
+    # ax.set_ylabel("y")
+
+    plt.savefig('./3.png', dpi = 100)
+    plt.show()
 
 if __name__ == "__main__":
-    mean = np.array([0,0])
-    cov_matrix = np.array([[1,-0.75],[-0.75,1]])
-    PlotBivGaussian(mean, cov_matrix)
+
+    prob = "3"
+
+    if prob == "2-b":
+        mean = np.array([0,0])
+        cov_matrix = np.array([[1,-0.75],[-0.75,1]])
+        PlotBivGaussian(mean, cov_matrix)
+    elif prob == "3":
+        PlotBoundary()
+
