3243e6f567d7ee37234437e0bdd0c8d2a5737576
Sai Bi <soundsilencebisai@gmail.com>
2016-02-28 14:10:25 -0800

    add: assign 6

--- assign6/code/prob1b.m
+++ assign6/code/prob1b.m
@@ -6,14 +6,44 @@ fid = fopen(class_path);
 class_info = textscan(fid, '%f %s');
 class_name = class_info{2};
 
-cluster_num = 10;
-idx = kmeans(data, cluster_num);
+% problem 1b
+if 0
+	cluster_num = 10;
+	idx = kmeans(data, cluster_num);
 
-for i = 1 : cluster_num
-	index = find(idx == i);
-	fprintf('class %d\n', i);
-	for j = 1 : length(index)
-		fprintf('%s ', class_name{index(j)});
+	for i = 1 : cluster_num
+		index = find(idx == i);
+		fprintf('class %d\n', i);
+		for j = 1 : length(index)
+			fprintf('%s ', class_name{index(j)});
+		end
+		fprintf('\n');
 	end
-	fprintf('\n');
+end
+
+% problem 1c
+if 0 
+	y = pdist(data);
+	z = linkage(y, 'average');
+	% leafOrder = optimalleaforder(z, y);
+	figure2 = figure;
+	dendrogram(z, 50, 'Labels', class_name, 'Orientation', 'right');
+	title('Problem 1-c: hierarchical clustering result on animal dataset.')
+	saveas(figure2, '1c.png');
+end
+
+if 1
+	coeff = pca(data);
+	coeff = coeff(:, 1:2);
+	new_data = data * coeff;
+	figure3 = figure;
+	
+	x = new_data(:, 1); y = new_data(:, 2);
+	scatter(x,y, 'filled');
+	text(x + 2, y + 2, class_name);
+	xlabel('First dimension');
+	ylabel('Second dimension');
+	title('Problem 4: data points after PCA');
+	saveas(figure3, '4.png');
+	
 end
